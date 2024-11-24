378479d3816c58a66c986819c72608194af99a2d
09014230 <85706754+Enterdatprisonfor445years@users.noreply.github.com>
2024-11-24 18:35:43 -0500

    4.14 Move Player Using Input

--- Argon_Assault/Assets/Scripts/Player Controller.cs
+++ Argon_Assault/Assets/Scripts/Player Controller.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float controlSpeed = 10f;
     void Update()
     {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
-        float xOffset = .1f;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float newXPos = transform.localPosition.x + xOffset;
 
-        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
     }
 }
